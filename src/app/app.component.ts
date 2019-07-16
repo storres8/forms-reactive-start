482ce1de4a4b228e9f5c1f8fe45ce4eee54067e1
storres8 <stevetorres7562@gmail.com>
2019-07-15 20:11:11 -0400

    created basics of form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupform: FormGroup;
+
+  ngOnInit() {
+    // new to initialize a new form group to create the form.
+    this.signupform = new FormGroup({
+      // form control can take up to 3 arguments with the 1st being the default value the field loads with
+      // the 2nd being an array of validators and the 3rd consists of any asynchronus validatos you
+      // may want to add. Ex with gender we passed in the default value of male when the form loads.
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl("male")
+    });
+  }
 }

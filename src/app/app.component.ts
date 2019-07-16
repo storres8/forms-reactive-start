490d58b5bfe3bc1be240392e3f760ff522f2ff2e
storres8 <stevetorres7562@gmail.com>
2019-07-15 20:12:05 -0400

    update.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
       // form control can take up to 3 arguments with the 1st being the default value the field loads with
       // the 2nd being an array of validators and the 3rd consists of any asynchronus validatos you
       // may want to add. Ex with gender we passed in the default value of male when the form loads.
+      // Each key value pair in our new FormGroup is called a form-control.
       username: new FormControl(null),
       email: new FormControl(null),
       gender: new FormControl("male")

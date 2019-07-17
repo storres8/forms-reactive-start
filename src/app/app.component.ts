70157aeb11065ad59afeaba630a608b4dd517260
storres8 <stevetorres7562@gmail.com>
2019-07-17 15:55:20 -0400

    created custom validator to check for forbidden names.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,24 @@ import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupform: FormGroup;
+  forbiddenUserNames = ["Chris", "Anna"];
 
   ngOnInit() {
     // new to initialize a new form group to create the form.
     this.signupform = new FormGroup({
       userData: new FormGroup({
-        // form control can take up to 3 arguments with the 1st being the default value the field loads with
-        // the 2nd being an array of validators and the 3rd consists of any asynchronus validatos you
-        // may want to add. Ex with gender we passed in the default value of male when the form loads.
-        // Each key value pair in our new FormGroup is called a form-control.
-        username: new FormControl(null, [Validators.required]),
+        /* form control can take up to 3 arguments with the 1st being the default value the field loads with
+        the 2nd being an array of validators and the 3rd consists of any asynchronus validatos you
+         may want to add. Ex with gender we passed in the default value of male when the form loads.
+        Each key value pair in our new FormGroup is called a form-control.
+        - import that we bind this to our custom validators b/c angular is the one that calles the method
+        upon validity. So we must pass a references to "this" so angualr knows how to properly execute the
+        validator.
+        */
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this)
+        ]),
         // It's important to remember that the validators that we pass in must be imported from angular forms
         // and that they are passed as reference, so we do not execute them. Instead angular itself will execute
         // our validators when something is typed into the value.
@@ -42,6 +50,24 @@ export class AppComponent implements OnInit {
     // we are accessing the form array and then getting all the individual from controls aka hobbies
     // that are stored in that array.
     return (<FormArray>this.signupform.get("hobbies")).controls;
+  }
+
+  /* the forbidedenNames function is our custum validator. Our validator takes in as an argument a control
+  with the value of form control. We also specify the return value of the function which will be a JS object
+  with the key being a string and the value being a boolean as shown bellow.
+
+  To access the value of the control that we are passing in we can use the .value method on our control and
+  angular will initiate it for us.
+
+  ***IMPORTANT*** if validation is successful then we MUST return either null or simply omit a retrun value
+  that demonstrates success. We only return something if validation is unsuccessful.
+  */
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    } else {
+      return null;
+    }
   }
 
   onSubmit() {
